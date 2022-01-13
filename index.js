0ac21b07a3b610893c60e3dfe09990d9df62d4c5
Jovanay <jovanay.d.carter@gmail.com>
2022-01-12 21:42:06 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,56 +1,17 @@
-function saturdayFun(fun) {
-    let activity;
-    
-    if (fun === undefined){
-      activity = 'roller-skate';
-    } else if (fun != undefined){
-      activity = fun;
-    }
-    let statement = `This Saturday, I want to ${activity}!`;
-  
-    return statement;
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`;
 }
-  
-const mondayWork = function (task) {
-    let activity;
-    
-    if (task === undefined){
-      activity = 'go to the office';
-    } else if (task != undefined){
-      activity = task;
-    }
-    let statement = `This Monday, I will ${activity}.`;
-  
-    return statement;
+
+const mondayWork = function (task = 'go to the office') {
+    return `This Monday, I will ${task}.`;
 }
-function wrapAdjective(outerParameter = '*'){
-    const inner = function (innerParameter = 'special'){
-        return `You are ${outerParameter}${innerParameter}${outerParameter}.`;
+
+let wrapAdjective = function(outerParameter = '*'){
+    return function (innerParameter = 'special'){
+        return `You are ${outerParameter}${innerParameter}${outerParameter}!`;
     };
-    return inner();
 }
 
 saturdayFun();
 mondayWork();
-wrapAdjective(); 
-
-/*function wrapAdjective(outerParameter){
-    let specialOperator;
-    let adj;
-
-    if (outerParameter === undefined){
-        specialOperator = '*';
-    } else if (outerParameter != undefined){
-        specialOperator = outerParameter;
-    }
-    const inner = function (innerParameter){
-        if (innerParameter === undefined){
-            adj = 'special';
-        } else if (innerParameter != undefined){
-            adj = innerParameter;
-        };
-        return `You are ${specialOperator}${innerParameter}${specialOperator}.`;
-    };
-    return inner();
-}
-*/
+wrapAdjective();
