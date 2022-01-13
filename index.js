99e9496d76d79c6534f9bcd66f4bbab1e75b95a5
Jovanay <jovanay.d.carter@gmail.com>
2022-01-12 20:59:41 -0500

    In-progress

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// code your solution here
+function saturdayFun(fun) {
+    let activity;
+    
+    if (fun === undefined){
+      activity = 'roller-skate';
+    } else if (fun != undefined){
+      activity = fun;
+    }
+    let statement = `This Saturday, I want to ${activity}!`;
+  
+    return statement;
+}
+  
+const mondayWork = function (task) {
+    let activity;
+    
+    if (task === undefined){
+      activity = 'go to the office';
+    } else if (task != undefined){
+      activity = task;
+    }
+    let statement = `This Monday, I will ${activity}.`;
+  
+    return statement;
+}
+ 
+/*function wrapAdjective(outerParameter){
+    let specialOperator;
+    let adj;
+
+    if (outerParameter === undefined){
+        specialOperator = '*';
+    } else if (outerParameter != undefined){
+        specialOperator = outerParameter;
+    }
+    const inner = function (innerParameter){
+        if (innerParameter === undefined){
+            adj = 'special';
+        } else if (innerParameter != undefined){
+            adj = innerParameter;
+        };
+        return `You are ${specialOperator}${innerParameter}${specialOperator}.`;
+    };
+    return inner();
+}
+*/
+
+function wrapAdjective(outerParameter = '*'){
+    const inner = function (innerParameter = 'special'){
+        return `You are ${outerParameter}${innerParameter}${outerParameter}.`;
+    };
+    return inner();
+}
+
+//saturdayFun();
+//mondayWork();
+//wrapAdjective();
